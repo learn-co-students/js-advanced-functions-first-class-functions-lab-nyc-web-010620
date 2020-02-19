a211a060d16fe071aed1d67c7bf7e531f825d98b
adam-xiao <adam.x.xiao@gmail.com>
2020-02-19 12:29:34 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function(list){
+    return list.slice(0, 2);
+}
+
+const returnLastTwoDrivers = function(list){
+    return list.slice(-2);
+}
+
+let selectingDrivers = [
+    returnFirstTwoDrivers,
+    returnLastTwoDrivers
+]
+
+function createFareMultiplier(num){
+    return function(fare){
+        return num * fare
+    }
+}
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+function selectDifferentDrivers(list, func){
+    if (func === returnFirstTwoDrivers){
+        return returnFirstTwoDrivers(list)
+    }else {
+        return returnLastTwoDrivers(list)
+    }
+}
